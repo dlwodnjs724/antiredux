123e41557ff9634b818652fbae6f35f9840404f9
이재원 <dlwodnjs724@naver.com>
2020-05-22 03:11:14 +0900

    #5 Updating the Store

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -3,19 +3,25 @@ import AppPresenter from './AppPresenter';
 import Store from 'store';
 
 class AppContainer extends Component {
-  state = {
-    message: 'Hello',
-    stuff: 10,
-    loggedIn: false,
-  };
+  constructor(props) {
+    super(props);
+    this._changeMessage = () => {
+      if (this.state.message === 'Hello') {
+        this.setState({
+          message: 'Bye bye',
+        });
+      } else {
+        this.setState({
+          message: 'Hello',
+        });
+      }
+    };
+    this.state = {
+      message: 'Hello',
+      changeMessage: this._changeMessage,
+    };
+  }
 
-  componentDidMount = () => {
-    setTimeout(() => {
-      this.setState({
-        message: 'Bye',
-      });
-    }, 2000);
-  };
   render() {
     return (
       <Store.Provider value={this.state}>
