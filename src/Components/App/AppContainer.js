390665ae971177f980f3c28bc09a292c187671c3
이재원 <dlwodnjs724@naver.com>
2020-05-22 02:49:44 +0900

    #4 Consuming the Store

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -3,9 +3,22 @@ import AppPresenter from './AppPresenter';
 import Store from 'store';
 
 class AppContainer extends Component {
+  state = {
+    message: 'Hello',
+    stuff: 10,
+    loggedIn: false,
+  };
+
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        message: 'Bye',
+      });
+    }, 2000);
+  };
   render() {
     return (
-      <Store.Provider>
+      <Store.Provider value={this.state}>
         <AppPresenter />
       </Store.Provider>
     );
